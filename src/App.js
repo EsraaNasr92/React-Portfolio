03063ea4cdf326126937b0a7bc713a56928caded
EsraaNasr92 <esraanasr706@gmail.com>
2024-05-15 14:22:48 +0300

    Added: Navbar section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Home from './Page/Home/Homescreen';
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Page/Home/Homescreen';
+import NavBar from './Page/Home/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
